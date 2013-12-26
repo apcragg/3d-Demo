8858907928024088703a1cdefd67160c0fad8a2c
apcragg <apcragg@gmail.com>
2013-12-26 13:22:44 -0800

    More git testing

--- Clean Project/src/engine/components/TransformComponent.java
+++ Clean Project/src/engine/components/TransformComponent.java
@@ -1,25 +1,47 @@
 package engine.components;
 
+import engine.main.Game;
+import engine.math.Transform;
+import engine.math.Vector3f;
+
 public class TransformComponent implements Component
 {
+	private Vector3f translation, rotation, scale;
+	
 	public TransformComponent()
 	{
-		
+		translation = new Vector3f();
+		rotation = new Vector3f();
+		scale = new Vector3f(1f, 1f, 1f);
 	}
 	
-	public void update() {
-		// TODO Auto-generated method stub
+	public void update() 
+	{
+		updateUniforms();
+	}
+	
+	public void updateUniforms()
+	{				
+		updateTransforms();
 		
+		Game.shader.uniformData4f("worldSpace", Transform.spatialMatrix());		
+	}
+	
+	public void updateTransforms()
+	{
+		Transform.setRotation(rotation);
+		Transform.setScale(scale);
+		Transform.setTranslation(translation);
 	}
 
-	public void render() {
-		// TODO Auto-generated method stub
-		
+	public void render() 
+	{
+		//on render do nothing
 	}
 
-	public void setup() {
-		// TODO Auto-generated method stub
-		
+	public void setup() 
+	{
+		//does nothing yet
 	}
 
 }
